45d4428b41a166938f6e61e9822ed79bcf736b79
Tom Phily <tom.phily@live.fr>
2016-02-01 14:22:06 +0100

    probleme de serialisation user facebook

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 // config/passport.js
 				
-// load all the things we need
+
 var LocalStrategy   = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 
@@ -16,26 +16,20 @@ var connection = mysql.createConnection(
 				  database: 'pictionnary'
 });	
 
-// expose this function to our app using module.exports
+
 module.exports = function(passport) 
 {
 
-	// =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
+	
     passport.serializeUser(function(user, done) 
     {
 		done(null, user.id);
     });
 
-    // used to deserialize the user
+
     passport.deserializeUser(function(id, done) 
     {
-		connection.query("select * from users where id = "+id,function(err,rows)
+		connection.query("select * from users where id="+id +";",function(err,rows)
 		{	
 			done(err, rows[0]);
 		});
@@ -53,45 +47,55 @@ module.exports = function(passport)
         console.log(profile);
         process.nextTick(function()
         {
-            var newUser = new Object();
-            newUser.profilepic = profile._json.picture;
-            newUser.prenom = profile._json.first_name;
-            newUser.email = profile._json.email;
-            newUser.birthdate = '00-00-00';
-            insertQuery = { prenom : newUser.prenom, profilepic : newUser.profilepic, email : newUser.email, birthdate : newUser.birthdate};
-            connection.query('INSERT INTO users SET ?', insertQuery,function(err,rows)
+            connection.query('Select * From users Where id=' + profile.id, function(err,rows)
+            {
+                if (err)
+                    return done(err);
+                if (rows.length) 
                 {
-                    if(err)
+                    return done(null, rows[0]);
+                } 
+                else
+                {
+                    var newUser = new Object();
+                    newUser.profilepic = profile._json.picture;
+                    newUser.prenom = profile._json.first_name;
+                    newUser.email = profile._json.email;
+                    newUser.id = profile._json.id;
+                    newUser.birthdate = '00-00-00';
+                    insertQuery = { id:newUser.id, prenom : newUser.prenom, profilepic : newUser.profilepic, email : newUser.email, birthdate : newUser.birthdate};
+                    connection.query('INSERT INTO users SET ?', insertQuery,function(err,rows2)
                     {
-                        return done(err);
-                    }
-                    else
-                    {
-                        newUser.id = rows.insertId;
-                        return done(null, newUser);
-                    }               
-                });
+                        if(err)
+                        {
+                            return done(err);
+                        }
+                        else
+                        {
+                            return done(null, newUser);
+                        }               
+                    });
+                }
+            });
         });
     }));
 
  	// =========================================================================
     // LOCAL SIGNUP ============================================================
     // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-	// by default, if there was no name, it would just be called 'local'
+
 
     passport.use('local-signup', new LocalStrategy(
     {
-        // by default, local strategy uses username and password, we will override with email
+
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
+        passReqToCallback : true 
     },
     function(req, email, password, done) 
     {
 
-		// find a user whose email is the same as the forms email
-		// we are checking to see if the user trying to login already exists
+
         connection.query("select * from users where email = '"+email+"'",function(err,rows){
 			console.log(rows);
 			console.log("above row object");
@@ -104,11 +108,10 @@ module.exports = function(passport)
             else 
             {
 
-				// if there is no user with that email
-                // create the user
+
                 var newUserMysql = new Object();			
 				newUserMysql.email    = email;
-                newUserMysql.password = password; // use the generateHash function in our user model
+                newUserMysql.password = password; 
 				newUserMysql.nom = req.body.nom;
 				newUserMysql.prenom = req.body.prenom;
 				newUserMysql.telephone = req.body.telephone;
@@ -141,18 +144,17 @@ module.exports = function(passport)
     // =========================================================================
     // LOCAL LOGIN =============================================================
     // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+
 
     passport.use('local-login', new LocalStrategy(
     {
-        // by default, local strategy uses username and password, we will override with email
+        
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
+        passReqToCallback : true 
     },
     function(req, email, password, done) 
-    { // callback with email and password from our form
+    { 
 
          connection.query("SELECT * FROM `users` WHERE `email` = '" + email + "'",function(err,rows)
          {
@@ -162,16 +164,15 @@ module.exports = function(passport)
             }
 			 if (!rows.length) 
 			 {
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, req.flash('loginMessage', 'No user found.')); 
             } 
 			
-			// if the user is found but the password is wrong
+
             if (!( rows[0].password == password))
             {
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); 
             }
 			
-            // all is well, return successful user
             return done(null, rows[0]);			
 		
 		});
